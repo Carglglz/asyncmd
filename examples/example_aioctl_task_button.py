75f25567abaec2559110cf8e3a4fee93dc676eb9
Carlosgg <carlosgilglez@gmail.com>
2023-01-27 17:26:45 +0000

    init aiotools

---
+++ examples/example_aioctl_task_button.py
@@ -0,0 +1,49 @@
+import uasyncio as asyncio
+import pyb
+import aiorepl
+import aioctl
+import random
+from aiolog import streamlog
+
+
+@aioctl.aiotask
+async def task_led(n, t, log=None):
+    i = 10 - n
+    while True:
+        await asyncio.sleep_ms(t)
+        pyb.LED(n).toggle()
+        await asyncio.sleep_ms(50)
+        pyb.LED(n).toggle()
+        if log:
+            log.info(f"[task_led_{n}] toggled LED {n}")
+        if n > 3:
+            i = round(i / (i - 1))
+    pyb.LED(n).off()
+    return random.random()
+
+
+@aioctl.aiotask
+async def task_button(log=None):
+    while True:
+        if pyb.Switch().value():
+            pyb.LED(3).toggle()
+            if log:
+                log.info("[task_button] button pressed!")
+        else:
+            pyb.LED(3).off()
+        await asyncio.sleep_ms(200)
+    return True
+
+
+# start the aiorepl task.
+
+
+async def main():
+    print("starting tasks...")
+    aioctl.set_log(streamlog)
+    aioctl.add(aiorepl.task, name="repl", prompt=">>> ")
+
+    await asyncio.gather(*aioctl.tasks())
+
+
+asyncio.run(main())
---
+++ examples/example_aioctl_task_schedule.py
@@ -0,0 +1,48 @@
+import aioctl
+import aioschedule
+import uasyncio as asyncio
+import aiorepl
+from aiolog import streamlog
+import upylog
+import time
+
+
+upylog.basicConfig(level="INFO", format="TIME_LVL_MSG", stream=streamlog)
+log = upylog.getLogger(
+    "pybV1.1", log_to_file=False, rotate=1000
+)  # This log to file 'error.log';
+
+
+aioctl.set_log(streamlog)
+
+
+@aioschedule.schedule_task(start_in=10, repeat=3600 * 25)
+@aioctl.aiotask
+async def test(a, b=1, alog=log):
+    await asyncio.sleep_ms(100)
+    alog.info(f"[test_{a}] start: hello {a}, {b}")
+    t0 = time.time()
+    while (time.time() - t0) < 5:
+        await asyncio.sleep(2)
+    alog.info(f"[test_{a}] done: hello {a}, {b}")
+    return 42
+
+
+async def main():
+    print("starting tasks...")
+
+    # start other program tasks.
+    aioctl.add(test, "world", b=10)
+    aioctl.add(test, "from", b=20, name="test_from", _id="test_from")
+    aioschedule.schedule(
+        "test_from", start_in=time.localtime(time.time() + 20), repeat=30
+    )
+    # start the aiorepl task.
+    aioctl.add(aiorepl.task, name="repl", prompt=">>> ")
+    # start the aiorepl toggle task
+    aioctl.add(aioschedule.schedule_loop, alog=None)
+
+    await asyncio.gather(*aioctl.tasks())
+
+
+asyncio.run(main())
