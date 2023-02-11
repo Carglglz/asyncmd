096f3369998253c46e5fac7626142f155711fc43
Carlosgg <carlosgilglez@gmail.com>
2023-02-11 20:26:59 +0000

    Fix ailog memory allocation and log rotation

--- aiolog.py
+++ aiolog.py
@@ -12,8 +12,6 @@ class AioStream(io.StringIO):
     def write(self, sdata):
         if self.tell() + len(sdata) > self._max_size:
             self.seek(0)
-            self._write(" " * self._max_size)
-            self.seek(0)
         self._write(sdata)
 
     def cat(self, grep=""):
@@ -21,13 +19,20 @@ class AioStream(io.StringIO):
         self.seek(0)
         # read and grep for regex
         if grep:
-            for line in self.read().strip().splitlines():
+            for line in self:
                 if line and "*" in grep and self._grep(grep, line):
-                    print(line)
+                    print(line, end="")
                 elif grep in line:
-                    print(line)
+                    print(line, end="")
+                if self.tell() >= index:
+                    self.seek(index)
+                    return
         else:
-            print(self.read().strip())
+            for line in self:
+                print(line, end="")
+                if self.tell() >= index:
+                    self.seek(index)
+                    return
         self.seek(index)
 
     def _grep(self, patt, line):
@@ -43,24 +48,60 @@ class AioStream(io.StringIO):
             try:
                 current_index = self.tell()
                 if current_index != init_index:
+                    self.seek(init_index)
                     if grep:
                         if "*" not in grep:
-                            for line in (
-                                self.getvalue()[init_index:].strip().splitlines()
-                            ):
+                            for line in self:
                                 if line and grep in line:
-                                    print(line)
+                                    print(line, end="")
+
+                                if self.tell() == current_index:
+                                    break
+                            if current_index < init_index:
+                                self.seek(0)
+                                init_index = 0
+                                for line in self:
+                                    if line and grep in line:
+                                        print(line, end="")
+
+                                    if self.tell() == current_index:
+                                        break
+
                         else:
-                            for line in (
-                                self.getvalue()[init_index:].strip().splitlines()
-                            ):
+                            for line in self:
                                 if line and self._grep(grep, line):
-                                    print(line)
+                                    print(line, end="")
+
+                                if self.tell() == current_index:
+                                    break
+
+                            if current_index < init_index:
+                                self.seek(0)
+                                init_index = 0
+                                for line in self:
+                                    if line and self._grep(grep, line):
+                                        print(line, end="")
+
+                                    if self.tell() == current_index:
+                                        break
 
                     else:
-                        for line in self.getvalue()[init_index:].strip().splitlines():
+                        for line in self:
                             if line:
-                                print(line)
+                                print(line, end="")
+
+                            if self.tell() == current_index:
+                                break
+
+                        if current_index < init_index:
+                            self.seek(0)
+                            init_index = 0
+                            for line in self:
+                                if line:
+                                    print(line, end="")
+
+                                if self.tell() == current_index:
+                                    break
 
                 init_index = current_index
                 await asyncio.sleep(wait)
