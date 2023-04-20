9b93cbeb82f547f2a13c75389d44be442527f5a5
Carlosgg <carlosgilglez@gmail.com>
2023-04-20 03:45:17 +0100

    Add rsyslogger and its logging handler.

---
+++ logging_handlers/rsysloghandler.py
@@ -0,0 +1,25 @@
+from logging import StreamHandler
+import sys
+
+
+class RsysLogHandler(StreamHandler):
+    def __init__(self, rsyslogger):
+        super().__init__(stream=None)
+        self.rsyslog = rsyslogger
+
+    def close(self):
+        super().close()
+
+    def emit(self, record):
+        if record.levelno >= self.level:
+            try:
+                # msg, level, timestamp, appname
+                self.format(record)
+                self.rsyslog.log_msg(
+                    record.message,
+                    record.levelname,
+                    record.asctime,
+                    record.name,
+                )
+            except Exception as e:
+                sys.print_exception(e)
