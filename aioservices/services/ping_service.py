0fb079a6825c0b942ad7d68e63335a885b647414
Carlosgg <carlosgilglez@gmail.com>
2023-10-26 21:28:05 +0100

    Add nodes kwarg to ping.service
    This allows to indicate n servers to ping.

--- aioservices/services/ping_service.py
+++ aioservices/services/ping_service.py
@@ -14,6 +14,7 @@ class PingService(Service):
         self.args = []
         self.kwargs = {
             "host": "localhost",
+            "nodes": [],
             "sleep": 60,
             "rx_size": 84,
             "on_stop": self.on_stop,
@@ -21,6 +22,8 @@ class PingService(Service):
         }
         self.log = None
         self._ping_stats = {}
+        self._ping_stats_nodes = {}
+        self._ping_lock = asyncio.Lock()
 
     def show(self):
         _stat_1 = ", ".join([f"{k}={v}" for k, v in self._ping_stats.items()])
@@ -28,6 +31,7 @@ class PingService(Service):
         # "info" to display
 
     def stats(self):
+        self._ping_stats.update(**self._ping_stats_nodes)
         return self._ping_stats
 
     def on_stop(self, *args, **kwargs):  # same args and kwargs as self.task
@@ -40,18 +44,60 @@ class PingService(Service):
         return e
 
     @aioctl.aiotask
-    async def task(self, host="localhost", sleep=60, rx_size=84, log=None):
+    async def task(self, host="localhost", nodes=[], sleep=60, rx_size=84, log=None):
         self.log = log
         await asyncio.sleep(1)
-        while True:
-            self._ping_stats = await ping(
-                host, quiet=True, rx_size=rx_size, loop=False, rtn_dict=True
-            )
+        # add ping child tasks to ping n hosts
+        # add per host stats
 
-            if log:
-                log.info(
-                    f"[ping.service] ping {host} OK @ {self._ping_stats.get('avg'):.0f} ms"
+        for node in nodes:
+            nodename = node.replace(".local", "")
+            if f"{self.name}.service.{nodename}" in aioctl.group().tasks:
+                aioctl.delete(f"{self.name}.service.{nodename}")
+            aioctl.add(
+                self.cping,
+                self,
+                host=node,
+                name=f"{self.name}.service.{nodename}",
+                _id=f"{self.name}.service.{nodename}",
+                on_stop=self.on_stop,
+                on_error=self.on_error,
+                log=log,
+            )
+            if self.log:
+                self.log.info(f"[{self.name}.service] cping @ {node} enabled")
+        self._ping_stats.update(host=host)
+        while True:
+            async with self._ping_lock:
+                self._ping_stats.update(
+                    **await ping(
+                        host, quiet=True, rx_size=rx_size, loop=False, rtn_dict=True
+                    )
                 )
+
+                if log:
+                    log.info(
+                        f"[ping.service] ping {host} OK @ {self._ping_stats.get('avg'):.0f} ms"
+                    )
+
+            await asyncio.sleep(sleep)
+
+    @aioctl.aiotask
+    async def cping(self, host="localhost", sleep=60, rx_size=84, log=None):
+        self.log = log
+        await asyncio.sleep(10)
+        self._ping_stats_nodes[host] = {}
+        while True:
+            async with self._ping_lock:
+                self._ping_stats_nodes[host].update(
+                    **await ping(
+                        host, quiet=True, rx_size=rx_size, loop=False, rtn_dict=True
+                    )
+                )
+                if log:
+                    log.info(
+                        f"[ping.service] ping {host} OK @ {self._ping_stats_nodes[host].get('avg'):.0f} ms"
+                    )
 
             await asyncio.sleep(sleep)
 
